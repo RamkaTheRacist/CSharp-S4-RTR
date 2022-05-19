0471b0bd843930bbff903d6cbabba197c5955bf2
Ramka <whereisourgod@gmail.com>
2022-05-19 14:13:34 +0300

    task4

---
+++ HW/task4/Program.cs
@@ -0,0 +1,20 @@
+﻿//В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+//Найти произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+int[] array = new int[123];
+void FillArray(int[] array)
+{
+    int length = array.Length;
+    int index = 0;
+    while(index < length)
+    {
+        array[index] = new Random().Next(1000); //чтоб хоть как-то работало
+        index++;
+    }
+}
+FillArray(array);
+int count = 0;
+for(int i = 0; i < array.Length; i++)
+{
+    if(array[i] > 9 && array[i] < 100) count++;
+}
+Console.WriteLine($"{count} numbers from 10 to 99 in array");
