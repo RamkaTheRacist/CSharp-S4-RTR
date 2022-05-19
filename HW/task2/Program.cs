130c043a17750c6603f7ae7b5c64105be2815bb4
Ramka <whereisourgod@gmail.com>
2022-05-19 13:47:03 +0300

    task2

---
+++ HW/task2/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задать массив из 12 элементов, заполненных числами из [0,9]. Найти сумму положительных/отрицательных элементов массива
+int[] array = new int[12];
+void FillArray(int[] array) // any name
+{
+    int length = array.Length;
+    int index = 0;
+    while(index < length)
+    {
+        array[index] = new Random().Next(0, 10);
+        index++;
+    }
+}
+FillArray(array);
+int sum = 0;
+for(int i = 0; i < array.Length; i++)
+{
+    sum = sum + array[i];
+}
+Console.WriteLine($"Result is {sum}");
