9c09119e46fa2c6cef2c98873aecf663a010a4ad
Ramka <whereisourgod@gmail.com>
2022-05-19 13:40:54 +0300

    task1

---
+++ HW/task1/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+int[] array = new int[8];
+void FillArray(int[] array) // any name
+{
+    int length = array.Length;
+    int index = 0;
+    while(index < length)
+    {
+        array[index] = new Random().Next(0, 2);
+        index++;
+    }
+}
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for(int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+FillArray(array);
+PrintArray(array);
