72748dd1d9b71a2b2a8eebfdd47798d972f5d6b3
Ramka <whereisourgod@gmail.com>
2022-05-19 12:27:27 +0300

    task5

---
+++ task5/Program.cs
@@ -0,0 +1,22 @@
+﻿//Посчитать количество вхождений элемента в массив
+int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 4, 1, 2, 5, 67, 123, 2};
+Console.WriteLine("Insert ur number");
+int number = int.Parse(Console.ReadLine());
+int count = array.Length;
+int any = 0;
+for (int  i = 0; i < count; i++)
+{
+    if(array[i] == number)
+    {
+        any++; 
+    }
+
+}
+if(any > 0)
+{
+    Console.WriteLine($"{number} is in array {any} times");
+}
+else
+{
+    Console.WriteLine($"{number} isnt in array");
+}
